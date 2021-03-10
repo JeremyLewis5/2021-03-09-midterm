d4ea4d735882446f950f4e7bcbd6808c50c0dc9e
JeremyLewis5 <jeremy.lewis@codeimmersives.com>
2021-03-10 11:31:48 -0600

    changes

--- main.js
+++ main.js
@@ -1,6 +1,49 @@
 // ┌─────────────────┐
 // │ Your code here! │	
 // └─────────────────┘
+function ageToAbilities(num){
+    const age = num;
+    if (age < 16) {
+        return "You can't drive.";
+    } 
+    if (age >= 16 && age <= 17) {
+         return 'You can drive but not vote.'
+     } 
+    if (age >= 18 && age <= 24) {
+        return 'You can vote but not rent a car.'
+    }
+    
+     if (age >= 25 ) { 
+        return 'You can do pretty much anything.';
+        }
+}
+
+function oddIndices(arr) {
+    const isOdd = arr % 2 == 1;
+    const arr1 = [];
+    if ([] ) {
+        return [];
+    }
+    if ([0]) {
+        return [0];
+    }
+    for (let i = 0; i < arr.length; i ++) {
+        const value = arr[i];
+        if (value === arr[i] % 2 == 1 ) {
+            arr1 === value
+        }
+
+        return arr1
+    }
+
+    
+    
+}
+
+function averageStringLength(arr) {
+    const str = '';
+    const length = str.length;
+}
 
 
 
